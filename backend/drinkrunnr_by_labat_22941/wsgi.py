f675dbe676e940d4380f05f453d734ee9cd81b69
Crowdbotics <team@crowdbotics.com>
2020-11-24 02:35:03 +0000

    Update models

--- backend/drinkrunnr_by_labat_22941/wsgi.py
+++ backend/drinkrunnr_by_labat_22941/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drinkrunnr_by_labat_22941.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drinkrunnr_by_labat_22941.settings")
 
 application = get_wsgi_application()
